3ea7c9634c5a227cddfbd301052cd4159780d10f
19023980 <19023980@myrp.edu.sg>
2021-04-20 10:59:59 +0800

    Image app

---
+++ app/src/main/java/sg/edu/rp/c347/id19023980/demoimageview/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c347.id19023980.demoimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView ivDay1;
+    ImageView ivDay2;
+    ImageView ivDay3;
+    ImageView ivDay4;
+    ImageView ivDay5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ivDay1 = findViewById(R.id.imageView);
+        ivDay1.setImageResource(R.drawable.day1);
+
+        ivDay2 = findViewById(R.id.imageView2);
+        ivDay2.setImageResource(R.drawable.day2);
+
+        ivDay3 = findViewById(R.id.imageView3);
+        ivDay3.setImageResource(R.drawable.day3);
+
+        ivDay4 = findViewById(R.id.imageView4);
+        ivDay4.setImageResource(R.drawable.day4);
+
+        ivDay5 = findViewById(R.id.imageView5);
+        ivDay5.setImageResource(R.drawable.day5);
+    }
+}
